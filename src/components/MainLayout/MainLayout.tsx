d59b22f762de6a81688a48ee20eb4f7c4c9cd15f
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2023-02-25 00:56:59 +0100

    updated app name, favicon, images mocks

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -9,8 +9,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/" underline="hover">
-        AWS practitioner Store
+      <Link color="inherit" href="/" underline="hover">
+        CloudX Courses Store
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            AWS practitioner Store
+            CloudX Courses Store
           </Link>
         </Typography>
 
